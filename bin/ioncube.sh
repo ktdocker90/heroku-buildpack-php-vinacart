b17999d0af00ad8da89a9c1d4d0184272d833d9d
hoangweb <kythuat.hoangweb@gmail.com>
2017-01-21 13:31:29 +0700

    commit the change

--- bin/ioncube.sh
+++ bin/ioncube.sh
@@ -7,15 +7,25 @@ install_ioncube_ext() {
   	local php_bin=`which php`
     #install_ext "ioncube" "automatic" "http://heroku-buildpack-php-with-ioncube.s3.amazonaws.com/ioncube.tar.gz"
     #exts+=("ioncube")
-    curl --silent --location "http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz" | tar xz -C $BUILD_DIR/.heroku/php
+    #curl --silent --location "http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz" | tar xz -C $BUILD_DIR/.heroku/php
+    mkdir -p $BUILD_DIR/.heroku/tmp
+    cd $BUILD_DIR/.heroku/tmp/
+
+    if [[ ! -d "$BUILD_DIR/.heroku/tmp/ioncube" ]]; then
+	    wget http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
+	    tar xvfz ioncube_loaders_lin_x86-64.tar.gz
+	    rm -rf xvfz ioncube_loaders_lin_x86-64.tar.gz
+	fi
+
     echo "- ioncube automatic; downloaded, using 'php.ini')" | indent
 	
-	ln -s $PHP_EXT_DIR/ioncube/ioncube_loader_lin_${PHP_VERSION%.*}.so $PHP_EXT_DIR/ioncube.so
-
+	
     local PHP_VERSION=$(php -r "echo PHP_MAJOR_VERSION.'.'.PHP_MINOR_VERSION;")
     PHP_VERSION="5.6"
     #cp "ioncube/ioncube_loader_lin_${PHP_VERSION}.so" "$extension_dir"
-
+    cp $BUILD_DIR/.heroku/tmp/ioncube/ioncube_loader_lin_${PHP_VERSION%.*}.so $PHP_EXT_DIR/ioncube.so
+	#ln -s $BUILD_DIR/.heroku/tmp/ioncube/ioncube_loader_lin_${PHP_VERSION%.*}.so $PHP_EXT_DIR/ioncube.so
+	
     sed -i '1 a zend_extension = "ioncube_loader_lin_'${PHP_VERSION}'.so"' ${BUILD_DIR}/.heroku/php/etc/php/php.ini
 
     # test
